452c698daa089e2f47b564e201cca78f3942fa42
Arik DeRapak <ariel.safar.tx@gmail.com>
2021-05-15 19:49:47 +0300

    Global config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { RedAlertModule } from './red-alert/red-alert.module';
 
 @Module({
-	imports: [RedAlertModule, ConfigModule.forRoot()],
+	imports: [RedAlertModule, ConfigModule.forRoot({ isGlobal: true })],
 	controllers: [],
 	providers: [],
 })
